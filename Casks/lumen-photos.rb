3b2cae28d2a8125d7905e8e3fbdad594b9d318d3
Seongil Park <zihado@gmail.com>
2026-06-08 22:14:37 +0900

    chore: release 0.2.0
    Bump app version to 0.2.0 and update the cask (version + sha256) for the
    v0.2.0 release zip.

--- Casks/lumen-photos.rb
+++ Casks/lumen-photos.rb
@@ -1,6 +1,6 @@
 cask "lumen-photos" do
-  version "0.1.0"
-  sha256 "e34f33d1892ceb6b8255547d63d3b00e79accb60b54ae17444c97067fa30fc7a"
+  version "0.2.0"
+  sha256 "bc69576172465ab022e6ee1444e585362dcdfbbb3379ae461afdc78dc737b820"
 
   url "https://github.com/rescenedev/lumen/releases/download/v#{version}/Lumen-#{version}.zip"
   name "Lumen"
