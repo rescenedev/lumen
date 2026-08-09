4b698b46e0ad984ad15ec5d00d19412f35f535a2
Seongil Park <zihado@gmail.com>
2026-08-09 16:25:14 +0900

    chore: release 0.5.12 (cask sha)

--- Casks/lumen-photos.rb
+++ Casks/lumen-photos.rb
@@ -1,6 +1,6 @@
 cask "lumen-photos" do
-  version "0.5.11"
-  sha256 "2707686c36670d03790e10f7ed5c8c2bea02398ae33443ba5e59f4c6da64378d"
+  version "0.5.12"
+  sha256 "9e8b70826c450fbb874a77b0643a2a3880636a18d7784421ad601bbed4f403f9"
 
   url "https://github.com/rescenedev/lumen/releases/download/v#{version}/Lumen-#{version}.zip"
   name "Lumen"
